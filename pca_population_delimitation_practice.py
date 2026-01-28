0796afa094faca364dcb6ef96ab4c4ffd91aec82
Jose Blanca <jblanca@upv.es>
2026-01-28 11:55:10 +0100

    add taxa pie chart and update versions

--- pca_population_delimitation_practice.py
+++ pca_population_delimitation_practice.py
@@ -21,7 +21,7 @@ def _():
     return PASSPORTS, PCA_RESULT, mo, numpy, pandas, px, scatter3d
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
     # Population delimitation practice
@@ -33,14 +33,12 @@ def _(mo):
 def _(PASSPORTS, pandas):
     passports = pandas.read_csv(PASSPORTS, index_col='id')
     passports["Country"] = passports["Country"].replace({"PER_N": "PER"})
-    passports
     return (passports,)
 
 
 @app.cell
 def _(PCA_RESULT, pandas):
     pca_res = pandas.read_csv(PCA_RESULT, index_col='id')
-    pca_res
     return (pca_res,)
 
 
@@ -53,6 +51,7 @@ def _(numpy, pandas, passports, pca_res, scatter3d):
     populations = scatter3d.Category(pandas.Series(numpy.full((data.shape[0], ), 'unclass'), index=data.index, name='populations'))
     populations.set_label_list(['unclass', 'pop1', 'pop2', 'pop3', 'pop4', 'pop5'])
     widget = scatter3d.Scatter3dWidget(data.iloc[:, :3].to_numpy(), category=taxa, point_ids=list(data.index))
+    widget.height = 800
     return countries, populations, taxa, widget
 
 
@@ -65,7 +64,6 @@ def _(countries, mo, populations, taxa):
 
 @app.cell
 def _(cat_dropdown, widget):
-    print(cat_dropdown.value.name)
     widget.category = cat_dropdown.value
     return
 
@@ -121,13 +119,39 @@ def _(pandas, passports):
         )
             pop_country_counts = country_counts[country_counts['population']==pop].loc[:, ('Country', 'n')]
         return pop_country_counts
-    return (count_countries_in_pop,)
+
+    def count_taxa_in_pop(populations, widget, pop):
+        classification = pandas.Series(populations.values.values, index=widget.point_ids, name='population')
+        class_taxon =pandas.merge(classification, passports.loc[:, 'Taxon'], left_index=True, right_index=True, how='left')
+        if pop == 'All':
+            pop_taxon_counts = class_taxon.loc[:, 'Taxon'].value_counts()
+            pop_taxon_counts = pandas.DataFrame({'Taxon':pop_taxon_counts.index, 'n': pop_taxon_counts})
+        else:
+            taxon_counts = (
+            class_taxon
+            .groupby(["population", "Taxon"])
+            .size()
+            .reset_index(name="n")
+        )
+            pop_taxon_counts = taxon_counts[taxon_counts['population']==pop].loc[:, ('Taxon', 'n')]
+        return pop_taxon_counts
+    return count_countries_in_pop, count_taxa_in_pop
+
+
+@app.cell
+def _(mo, pop_dropdown):
+    mo.md(f"""
+    ## Population composition: {pop_dropdown.value}
+    """)
+    return
 
 
 @app.cell
 def _(
     count_countries_in_pop,
+    count_taxa_in_pop,
     get_pop_tick,
+    mo,
     pop_dropdown,
     populations,
     px,
@@ -137,16 +161,46 @@ def _(
     _ = get_pop_tick()
 
     pop_country_counts = count_countries_in_pop(populations, widget, pop_dropdown.value)
+    pop_taxon_counts = count_taxa_in_pop(populations, widget, pop_dropdown.value)
 
-    fig = px.choropleth(
+    taxon_pie_fig = px.pie(pop_taxon_counts, values='n', names='Taxon')
+
+    geo_fig = px.choropleth(
         pop_country_counts,
         locations="Country",
         locationmode="ISO-3",
         color="n",
         color_continuous_scale="Viridis",
-        title="Number of individuals by country",
     )
-    fig
+    mo.hstack(items=[taxon_pie_fig, geo_fig], widths=[1.5, 3])
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ## Passports
+    """)
+    return
+
+
+@app.cell
+def _(passports):
+    passports
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ## PCA projections
+    """)
+    return
+
+
+@app.cell
+def _(pca_res):
+    pca_res
     return
 
 
